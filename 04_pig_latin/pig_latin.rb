042e9014938ee0b13ecd782108115633f1866cd8
Edmilson <edmilsonrobson@gmail.com>
2016-11-10 23:59:05 -0300

    adds new test case for exercise 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,16 +4,23 @@ def translate(msg)
 	new_message = ""
 	vowels = ["a", "e", "i", "o", "u"]
 
-	msg.scan(/[\w]+/).each do |word|
+	msg.scan(/[\w]+/).map! do |word|
+		capitalized_word = (word[0] == word[0].upcase)
+
 		until (vowels.include?(word[0]))
-			if (word[0..1] == "qu")
-				word += word[0..1]
+			if (word[0..1].downcase == "qu")
+				word += word[0..1].downcase
 				word[0..1] = ""
 			else
-				word += word[0]
+				word += word[0].downcase
 				word[0] = ""
-			end
+			end		
 		end		
+
+		# First letter now is guaranteed to be a vowel
+		if capitalized_word
+			word[0] = word[0].upcase
+		end
 		word = word + "ay"
 		new_message += word + " "
 	end
