7e4de390e817ce2def7ddbd622336c7cb4558007
Edmilson <edmilsonrobson@gmail.com>
2016-11-11 00:34:06 -0300

    Adds final exercise solution, 06! :)

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,25 @@
 class Timer
-  #write your code here
+	attr_accessor :seconds, :minutes, :hours
+  	#write your code here
+
+def initialize
+	@seconds = 0
+	@minutes = 0
+	@hours = 0
+end
+
+def time_string
+	while @seconds > 60
+		@seconds -= 60
+		@minutes += 1
+	end
+
+	while @minutes > 60
+		@minutes -= 60
+		@hours += 1
+	end
+
+	"%02d:%02d:%02d" % [hours, minutes, seconds]
+end
+
 end
