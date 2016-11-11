c53e396bfc2492a80baaa5208db6423fe8e9a31d
Edmilson <edmilsonrobson@gmail.com>
2016-11-10 23:46:43 -0300

    Adds solution to exercise 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
 #write your code here
+
+def translate(msg)
+	new_message = ""
+	vowels = ["a", "e", "i", "o", "u"]
+
+	msg.scan(/[\w]+/).each do |word|
+		until (vowels.include?(word[0]))
+			if (word[0..1] == "qu")
+				word += word[0..1]
+				word[0..1] = ""
+			else
+				word += word[0]
+				word[0] = ""
+			end
+		end		
+		word = word + "ay"
+		new_message += word + " "
+	end
+
+	new_message.rstrip
+end
