f7bcff31a5dbe1048f70eac4c210c13f6deda7c7
Edmilson <edmilsonrobson@gmail.com>
2016-11-11 00:18:15 -0300

    Adds the solution to the exercise 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,28 @@
 class Book
+	attr_accessor :title
 # write your code here
+
+def initialize	
+	@title = ""
+end
+
+def title
+	formatted_title = ""
+	exceptions = ["and", "in", "the", "of", "a", "an"]
+
+	word_list = @title.scan(/[\w]+/)
+
+	word_index = 0
+	word_list.each do |word|
+		if exceptions.include?(word) == false or word_index == 0
+			word = word[0].upcase + word[1..word.length]
+		end
+		formatted_title += " " + word
+
+		word_index += 1
+	end
+	
+	formatted_title.lstrip
+end
+
 end
